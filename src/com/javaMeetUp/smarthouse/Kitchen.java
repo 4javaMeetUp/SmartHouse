7a17006cdc52044002a7d1e7cc4567d9f7ad9335
Eduard Streltsov <streltsov.eduard@gmail.com>
2017-01-29 19:10:29 +0200

    Add Kitchen class

---
+++ src/com/javaMeetUp/smarthouse/Kitchen.java
@@ -0,0 +1,21 @@
+package com.javaMeetUp.smarthouse;
+
+public class Kitchen implements Command {
+
+    private boolean lightIsOn;
+
+    public Kitchen() {
+        this.lightIsOn = false;
+    }
+
+    public void turnTheLight() {
+        this.lightIsOn = !lightIsOn;
+    }
+
+    @Override
+    public void execute() {
+        turnTheLight();
+        System.out.println("Light in the kitchen is " + (lightIsOn ? "on" : "off") + ".");
+    }
+
+}

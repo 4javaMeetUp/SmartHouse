6dddbcc6362d1a2e27b83eedc18e865ff46158e8
andrew_bandura <andrew_bandura@gmail.com>
2017-01-29 19:37:26 +0200

    State added to Command

--- src/com/javaMeetUp/smarthouse/Command.java
+++ src/com/javaMeetUp/smarthouse/Command.java
@@ -2,7 +2,7 @@ package com.javaMeetUp.smarthouse;
 
 
 public interface Command {
-    boolean SwitchedOn = false;
+    boolean switchedOn = false;
     public void execute();
     public boolean getSwitchedOn();
 }
--- src/com/javaMeetUp/smarthouse/DishWasher.java
+++ src/com/javaMeetUp/smarthouse/DishWasher.java
@@ -3,5 +3,16 @@ package com.javaMeetUp.smarthouse;
 /**
  * Created by Andrew on 29.01.2017.
  */
-public class DishWasher {
+public class DishWasher implements Command {
+
+    boolean switchedOn;
+
+    public void execute() {
+        switchedOn = !switchedOn;
+        System.out.println("Current state ="+switchedOn);
+    }
+
+    public boolean getSwitchedOn() {
+        return switchedOn;
+    }
 }

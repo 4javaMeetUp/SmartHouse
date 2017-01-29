896118b86f0bb9a04b4c09fd1354118e8ed116bb
Vitaliy Rybakov <rwmail3@gmail.com>
2017-01-29 19:18:31 +0200

    Add Class TV for work

---
+++ src/com/javaMeetUp/smarthouse/TV.java
@@ -0,0 +1,27 @@
+package com.javaMeetUp.smarthouse;
+
+/**
+ * Created by rw on 29.01.2017.
+ */
+public class TV implements Command{
+
+
+    //Характеристики телевизора
+    //вк
+    public int diagonal;
+
+    //включение телевизара
+
+    //статус (вкоючен выключен)
+    boolean isTurnOff;
+
+    @Override
+    public void execute() {
+        System.out.println("Телевизор существует");
+    }
+
+
+    //Громкость уровень
+
+    //
+}

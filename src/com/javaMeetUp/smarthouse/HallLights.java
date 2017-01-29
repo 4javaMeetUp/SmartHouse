d76c7b65676fee63c9cf524d6d61ad8aafc7058b
Denis Tantsev <dtantsev@gmail.com>
2017-01-29 19:13:15 +0200

    Something added...

---
+++ src/com/javaMeetUp/smarthouse/HallLights.java
@@ -0,0 +1,11 @@
+package com.javaMeetUp.smarthouse;
+
+/**
+ * @author Denis Tantsev (dtantsev@gmail.com)
+ */
+public class HallLights implements Command {
+    @Override
+    public void execute() {
+        System.out.println("Light is ... i don't know");
+    }
+}

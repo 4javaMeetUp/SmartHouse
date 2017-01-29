d1e7c69d2e9aeb0530d202ae7270282bb97ffedd
Denis Tantsev <dtantsev@gmail.com>
2017-01-29 19:37:42 +0200

    Added some changes

--- src/com/javaMeetUp/smarthouse/HallLights.java
+++ src/com/javaMeetUp/smarthouse/HallLights.java
@@ -4,8 +4,14 @@ package com.javaMeetUp.smarthouse;
  * @author Denis Tantsev (dtantsev@gmail.com)
  */
 public class HallLights implements Command {
+    boolean state;
+    private void changeState() {
+        state = !state;
+    }
+
     @Override
     public void execute() {
-        System.out.println("Light is ... i don't know");
+        changeState();
+        System.out.println("Light state in hall changed");
     }
 }
